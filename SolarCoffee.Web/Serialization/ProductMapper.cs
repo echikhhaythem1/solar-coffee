9b5b02d9ca73dec45c3a112aa1cbfeaf3b4fe4b3
echikhhaythem1 <echikhhaythem20@gmail.com>
2021-02-22 20:52:42 +0100

    Implement Data mapper

---
+++ SolarCoffee.Web/Serialization/ProductMapper.cs
@@ -0,0 +1,52 @@
+﻿using SolarCoffee.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarCoffee.Web.Serialization
+{
+    public class ProductMapper
+    {
+        /// <summary>
+        /// Maps a product data model to ProductModel view model
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static ProductModel SerializeProductModel (Data.Models.Product product)
+        {
+            return new ProductModel
+            {
+         Id = product.Id,
+        CreatedOn = product.CreatedOn,
+        UpdatedOn = product.UpdatedOn,
+         Name =product.Name,
+        Description= product.Description,
+         Price = product.Price,
+         IsTaxable = product.IsTaxable,
+         IsArchived = product.IsArchived
+
+    };
+}
+        /// <summary>
+        /// Maps  ProductModel view model to a product data model 
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public static Data.Models.Product SerializeProductModel(ProductModel product)
+        {
+            return new Data.Models.Product
+            {
+                Id = product.Id,
+                CreatedOn = product.CreatedOn,
+                UpdatedOn = product.UpdatedOn,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                IsTaxable = product.IsTaxable,
+                IsArchived = product.IsArchived
+
+            };
+        }
+    }
+}
